36a62bbe5b4a3a7dd55f0c327c09cc9e5e0a0cd0
davigura <davi_gura@estudante.sesisenai.org.br>
2026-04-02 11:03:39 -0300

    add- adicionei um easteregg

--- calculadora.js
+++ calculadora.js
@@ -16,4 +16,21 @@ function resultado() {
         const display = document.getElementById('display')
         display.value = display.value.slice(0,-1)
     }
+function resultado() {
+    let display = document.getElementById("display");
+
+    try {
+        let conta = display.value.replace(",", "."); // pra funcionar com vírgula
+        let resultado = eval(conta);
+
+        if (resultado == 67) {
+            display.value = "feijao com farinha";
+        } else {
+            display.value = resultado;
+        }
+
+    } catch {
+        display.value = "Erro";
+    }
+}
 
